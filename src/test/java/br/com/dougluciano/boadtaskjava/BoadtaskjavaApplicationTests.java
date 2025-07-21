dda7332e097ed3e5040e209e171a2595875af176
Douglas O. Luciano <douglas.luciano2@gmail.com>
2025-07-21 14:18:03 -0300

    Setup: Inicializar projeto Spring Boot com dependências

---
+++ src/test/java/br/com/dougluciano/boadtaskjava/BoadtaskjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dougluciano.boadtaskjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoadtaskjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
